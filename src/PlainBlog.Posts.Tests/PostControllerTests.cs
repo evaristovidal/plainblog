f60dc4d06b979626e9ae2c4ca8183b2d65108e84
Evaristo Vidal <evidal@PROPHIX.com>
2024-04-03 17:18:04 +0200

    werwer

--- src/PlainBlog.Posts.Tests/PostControllerTests.cs
+++ src/PlainBlog.Posts.Tests/PostControllerTests.cs
@@ -1,21 +1,35 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Moq;
+using PlainBlog.Posts.Api;
+
 namespace PlainBlog.Posts.Tests;
 
 [Trait("Category", nameof(PostControllerTests))]
 public class PostControllerTests
 {
+    private readonly PostController _controller;
+    private readonly Mock<ILogger<PostController>> _loggertMock = new();
+
     public PostControllerTests()
     {
+        var serviceCollection = new ServiceCollection();
+        serviceCollection.AddSingleton(_loggertMock.Object);
+        serviceCollection.BuildServiceProvider();
 
+        _controller = new PostController(_loggertMock.Object);
     }
 
     [Fact]
-    public void GetAsync_ShouldReturnPosts()
+    public async Task GetAsync_ShouldReturnPosts()
     {
         // Arrange
 
         // Act
+        var result = await _controller.GetAsync(CancellationToken.None);
 
         // Assert
-        Assert.True(true);
+        Assert.IsType<OkObjectResult>(result);
     }
 }
